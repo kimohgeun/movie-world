3e0b3845176be1b3e4fb101931abe9e607e2ee81
kimohgeun <kog1059@gmail.com>
2019-07-16 18:15:37 +0900

    Updates

---
+++ precache-manifest.cc06a56ab6ef48d8564427038f8a397b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e145e4a30442c7114732b3ca9c3bac",
+    "url": "/movie-world/index.html"
+  },
+  {
+    "revision": "abd7064f1ee6f2f78256",
+    "url": "/movie-world/static/js/2.318cc96b.chunk.js"
+  },
+  {
+    "revision": "ad5abf506ebed5be7a4b",
+    "url": "/movie-world/static/js/main.190a5c9d.chunk.js"
+  },
+  {
+    "revision": "207aec8937300801bd2d",
+    "url": "/movie-world/static/js/runtime~main.29234f5c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/movie-world/precache-manifest.b17d24e0b8b1b4789c9b8e68aafc402b.js"
+  "/movie-world/precache-manifest.cc06a56ab6ef48d8564427038f8a397b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/movie-world/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-world/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
